5daa7be104a8f1aacc57aa99196e560ab321bb4d
sofiamarquez <54717228+sofiamarquez@users.noreply.github.com>
2019-11-03 20:04:33 -0600

    Add files via upload

---
+++ Proyecto/boss/Boss.java
@@ -0,0 +1,25 @@
+import personaje.*;
+
+
+
+public abstract class Boss extends Personaje{
+
+private Habilidad[] habilidades;
+
+
+public Boss(String nombre, int hp, int ataque, int defensa){
+		super(nombre, hp, ataque, defensa);
+
+}
+
+public Habilidad[] getHabilidades(){
+		return habilidades;
+	}
+public void setHabilidades(Habilidad[] habilidades){
+		this.habilidades=habilidades;
+	}
+
+
+
+		
+}
