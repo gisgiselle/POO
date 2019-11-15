8ddb81aa0067aa0304824523f8eeb4757b59f0a0
gisgiselle <47361500+gisgiselle@users.noreply.github.com>
2019-11-15 15:13:06 -0600

    Add files via upload

---
+++  Excepciones/NegativaException.java
@@ -0,0 +1,9 @@
+
+public class NegativaException extends Exception{
+	
+	public NegativaException(){
+		System.out.println("No puede ser negativo");
+	}
+
+
+}
---
+++  Excepciones/TextVacioException.java
@@ -0,0 +1,5 @@
+public class TextVacioException extends Exception{
+        public TextVacioException(){
+                System.out.println("Llena el espacio");
+		}
+}
