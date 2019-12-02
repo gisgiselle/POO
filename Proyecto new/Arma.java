9b8d6aebbd2931dce6c5f0c23dd473823262c6a7
Giselle <brengimolina@gmail.com>
2019-12-02 15:36:06 -0600

    Delete Arma.java

--- Proyecto new/Arma.java
+++
@@ -1,9 +0,0 @@
-
-public class Arma extends Objeto implements Material{
-	public Arma(String nombre, int puntos, int menosE){
-		super(nombre,puntos,menosE);
-	}
-
-
-	//public abstract void damage();
-}
