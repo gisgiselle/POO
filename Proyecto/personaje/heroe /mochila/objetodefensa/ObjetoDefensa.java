a38e52ea7e5f8a7a5531b13aea27af03b0d2435d
Giselle <brengimolina@gmail.com>
2019-12-02 19:38:13 -0600

    Delete ObjetoDefensa.java

--- Proyecto/personaje/heroe /mochila/objetodefensa/ObjetoDefensa.java
+++
@@ -1,9 +0,0 @@
-package personaje.Heroe.Mochila.ObjetoDefensa;
-import mochila.*;
-import personaje.*;
-public abstract class ObjetoDefensa extends Objeto{
-	public ObjetoDefensa(String nombre, int puntos){
-		super(nombre,puntos);
-	}
-	public abstract double protect();
-}
