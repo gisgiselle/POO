4a66098635942fcd3a629e10804b580d8f507a05
Giselle <brengimolina@gmail.com>
2019-12-02 19:38:41 -0600

    Delete Arma.java

--- Proyecto/personaje/heroe/mochila/arma/Arma.java
+++
@@ -1,9 +0,0 @@
-package personaje.heroe.mochila.arma;
-import mochila.*
-public abstract class Arma extends Objeto{
-	public Arma(String nombre, int puntos){
-		super(nombre,puntos);
-	}
-
-	public abstract double damage();
-}
